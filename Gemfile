10a2c26d9ac0d63e7060950e5c0172b036b42c43
Meng-Hsin Tung <itswindtw@gmail.com>
2015-03-05 13:05:52 -0500

    Bootstraping database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'slim'
 
+gem 'sqlite3'
+gem 'sequel'
+
 group :development do
   gem 'rerun'
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,11 @@
 # Database
+require 'sequel'
+require 'yaml'
+db_config = YAML.load_file(File.join(BASE_PATH, 'config/database.yml'))[RACK_ENV]
+DB = Sequel.connect(db_config)
+
+# Sequel.extension :migration
+# Sequel::Migrator.check_current(DB, File.join(BASE_PATH, 'db/migrations'))
 
 # Logging
 # env['rack.logger']
