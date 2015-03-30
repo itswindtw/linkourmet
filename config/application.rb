03f7a070e78cb1f6894b47817a12934b0ebe96a2
Meng-Hsin Tung <itswindtw@gmail.com>
2015-03-30 18:31:23 -0400

    RedisCloud

--- config/application.rb
+++ config/application.rb
@@ -4,15 +4,13 @@ Sequel::Migrator.check_current(DB, File.join(BASE_PATH, 'db/migrations'))
 # Models
 require 'models'
 
-# Redis
-require 'redis'
-if ENV['REDISCLOUD_URL']
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
-
 # Resque
 require 'resque'
+Resque.redis = if RACK_ENV == 'production'
+  ENV['REDISCLOUD_URL']
+else
+  'localhost:6379'
+end
 
 Resque.before_fork do
   defined?(DB) && DB.disconnect
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,12 +9,14 @@ before_fork do |server, worker|
   end
 
   defined?(DB) && DB.disconnect
-
-  @sidekiq_pid ||= spawn('env TERM_CHILD=1 QUEUE=* bundle exec rake resque:work')
+  Resque.redis.quit
+  @resque_pid ||= spawn('env TERM_CHILD=1 QUEUE=* bundle exec rake resque:work')
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
+
+  Resque.redis = ENV['REDISCLOUD_URL']
 end
